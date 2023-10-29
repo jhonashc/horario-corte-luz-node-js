cac63dc72451b7777b248faafa752eeabb92118b
jhonashc <jhonhuiracocha123456789@gmail.com>
2023-10-28 23:22:56 -0500

    schedule service added: get schedules with pagination added

---
+++ src/services/schedule.service.ts
@@ -0,0 +1,40 @@
+import { PrismaClient, Schedule } from "@prisma/client";
+
+import { PaginationDto } from "../dtos";
+
+import { CustomError } from "../errors/custom.error";
+
+export class ScheduleService {
+  private readonly prisma: PrismaClient;
+
+  constructor() {
+    this.prisma = new PrismaClient();
+  }
+
+  public async getSchedules(paginationDto: PaginationDto) {
+    try {
+      const { page, limit } = paginationDto;
+
+      const findScheduleCounter = this.prisma.schedule.count();
+
+      const findSchedules = await this.prisma.schedule.findMany({
+        take: limit,
+        skip: (page - 1) * limit,
+      });
+
+      const [totalItems, schedules] = await Promise.all([
+        findScheduleCounter,
+        findSchedules,
+      ]);
+
+      return {
+        page,
+        limit,
+        totalItems,
+        data: schedules,
+      };
+    } catch (error) {
+      throw CustomError.internalServer(`${error}`);
+    }
+  }
+}
