08b139db4609d289b30e091377a6acfd1e2be48c
jhonashc <jhonhuiracocha123456789@gmail.com>
2023-10-31 14:51:33 -0500

    auth controller updated: login and register user refactored with new dtos and middleware for exceptions

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -1,43 +1,27 @@
-import { Request, Response } from "express";
+import { NextFunction, Request, Response } from "express";
 
 import { LoginUserDto, RegisterUserDto } from "../dtos";
 
 import { AuthService } from "../services";
 
-import { CustomError } from "../errors/custom.error";
-
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
-  private handleError = (error: unknown, res: Response) => {
-    if (error instanceof CustomError) {
-      return res.status(error.statusCode).json({ error: error.message });
-    }
-
-    return res
-      .status(500)
-      .json({ error: "Algo salió mal, inténtalo de nuevo más tarde" });
-  };
-
-  registerUser = (req: Request, res: Response) => {
-    const [error, registerDto] = RegisterUserDto.create(req.body);
-
-    if (error) return res.status(400).json({ error });
+  registerUser = (req: Request, res: Response, next: NextFunction) => {
+    const { username, email, password } = req.body as RegisterUserDto;
 
     this.authService
-      .registerUser(registerDto!)
+      .registerUser({ username, email, password })
       .then((user) => res.json(user))
-      .catch((error) => this.handleError(error, res));
+      .catch((error) => next(error));
   };
 
-  loginUser = (req: Request, res: Response) => {
-    const [error, loginDto] = LoginUserDto.create(req.body);
-
-    if (error) return res.status(400).json({ error });
+  loginUser = (req: Request, res: Response, next: NextFunction) => {
+    const { email, password } = req.body as LoginUserDto;
 
     this.authService
-      .loginUser(loginDto!)
+      .loginUser({ email, password })
       .then((user) => res.json(user))
-      .catch((error) => this.handleError(error, res));
+      .catch((error) => next(error));
   };
 }
