fca7d342db249f6ffff62668f831028e0266db4e
jhonashc <jhonhuiracocha123456789@gmail.com>
2023-10-31 14:50:08 -0500

    custom middlewares added: exception handler middleware implemented

--- src/app.ts
+++ src/app.ts
@@ -4,6 +4,8 @@ import express, { Application } from "express";
 
 import { envs } from "./config/envs";
 
+import { exceptionHandler } from "./middlewares";
+
 import { AppRoutes } from "./routes/app.route";
 
 class Server {
@@ -16,20 +18,25 @@ class Server {
 
     this.middlewares();
     this.routes();
+    this.customMiddlewares();
   }
 
-  private middlewares() {
+  private middlewares(): void {
     this.app.use(cors());
-    this.app.use(morgan("tiny"));
+    this.app.use(morgan("dev"));
     this.app.use(express.json());
     this.app.use(express.urlencoded({ extended: true }));
   }
 
-  private routes() {
+  private customMiddlewares(): void {
+    this.app.use(exceptionHandler);
+  }
+
+  private routes(): void {
     this.app.use(AppRoutes.routes);
   }
 
-  listen() {
+  listen(): void {
     this.app.listen(this.port, () => {
       console.log(`🚀 App running on port ${this.port}`);
       console.log(`Running in ${envs.NODE_ENV}`);
