34e02d65f6cfa3b62c320171f64b74e58f44c397
jhonashc <jhonhuiracocha123456789@gmail.com>
2023-10-31 16:28:47 -0500

    schedule controller updated: middleware to validate the request using Joi schemas added

--- src/controllers/index.ts
+++ src/controllers/index.ts
@@ -1,2 +1,3 @@
 export * from "./auth.controller";
+
 export * from "./schedule.controller";
--- src/controllers/schedule.controller.ts
+++ src/controllers/schedule.controller.ts
@@ -1,37 +1,23 @@
-import { Request, Response } from "express";
+import { NextFunction, Request, Response } from "express";
 
-import { PaginationDto } from "../dtos";
+import { GetSchedulesDto } from "../dtos";
 
 import { ScheduleService } from "../services";
-
-import { CustomError } from "../errors/custom.error";
 
 export class ScheduleController {
   constructor(private readonly scheduleService: ScheduleService) {}
 
-  private handleError = (error: unknown, res: Response) => {
-    if (error instanceof CustomError) {
-      return res.status(error.statusCode).json({ error: error.message });
-    }
-
-    return res
-      .status(500)
-      .json({ error: "Algo salió mal, inténtalo de nuevo más tarde" });
-  };
-
-  getSchedules = (req: Request, res: Response) => {
-    const { page = 1, limit = 10 } = req.query;
-
-    const city = req.query.city as string;
-    const sector = req.query.sector as string;
-
-    const [error, paginationDto] = PaginationDto.create(+page, +limit);
-
-    if (error) return res.status(400).json({ error });
+  getSchedules = (req: Request, res: Response, next: NextFunction) => {
+    const { city, sector, page, limit } = req.query as GetSchedulesDto;
 
     this.scheduleService
-      .getSchedules(paginationDto!, city, sector)
+      .getSchedules({
+        city,
+        sector,
+        page: page && +page,
+        limit: limit && +limit,
+      })
       .then((schedules) => res.json(schedules))
-      .catch((error) => this.handleError(error, res));
+      .catch((error) => next(error));
   };
 }
