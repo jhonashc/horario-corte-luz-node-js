abe70b83a7d42a3c9b0928a60ecb23ea5080aa16
jhonashc <jhonhuiracocha123456789@gmail.com>
2023-10-31 14:46:51 -0500

    validate request middleware for validate joi schemas added

---
+++ src/middlewares/validate-request.middleware.ts
@@ -0,0 +1,29 @@
+import { NextFunction, Request, Response } from "express";
+
+import { Schema } from "joi";
+
+import { CustomError } from "../errors/custom.error";
+
+export type RequestType = "body" | "params" | "query";
+
+export const validateRequest = (schema: Schema, requestType: RequestType) => {
+  return (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const { error } = schema.validate(req[requestType]);
+
+      if (error) {
+        const { details } = error;
+
+        const message: string = details
+          .map((detail) => detail.message.replace(/['"]/g, ""))
+          .join(",");
+
+        throw CustomError.badRequest(message);
+      }
+
+      next();
+    } catch (error) {
+      next(error);
+    }
+  };
+};
