619d13b5252b1db98cca8cb6efb155bfbf654c13
jhonashc <jhonhuiracocha123456789@gmail.com>
2023-10-31 14:48:52 -0500

    auth dtos updated: now classes are interfaces

--- src/dtos/auth/login-user.dto.ts
+++ src/dtos/auth/login-user.dto.ts
@@ -1,16 +1,4 @@
-import { regularExps } from "../../helpers";
-
-export class LoginUserDto {
-  private constructor(public email: string, public password: string) {}
-
-  static create(object: { [key: string]: any }): [string?, LoginUserDto?] {
-    const { email, password } = object;
-
-    if (!email) return ["Debe ingresar un correo electrónico"];
-    if (!regularExps.email.test(email))
-      return ["El correo electrónico no es válido"];
-    if (!password) return ["Debe ingresar una contraseña"];
-
-    return [undefined, new LoginUserDto(email, password)];
-  }
+export interface LoginUserDto {
+  email: string;
+  password: string;
 }
