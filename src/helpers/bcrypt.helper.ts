7a13124e93ea6e27978b2dc66209eb00acb5efba
jhonashc <jhonhuiracocha123456789@gmail.com>
2023-11-02 00:37:12 -0500

    bcrypt helper updated: is now a class that exposes static methods

--- src/helpers/bcrypt.helper.ts
+++ src/helpers/bcrypt.helper.ts
@@ -1,13 +1,15 @@
 import bcrypt, { genSaltSync } from "bcrypt";
 
-export const encryptPassword = (password: string): Promise<string> => {
-  const salt = genSaltSync();
-  return bcrypt.hash(password, salt);
-};
+export class BcryptHelper {
+  static encryptPassword(password: string): Promise<string> {
+    const salt = genSaltSync();
+    return bcrypt.hash(password, salt);
+  }
 
-export const comparePasswords = (
-  password: string,
-  receivedPassword: string
-): Promise<boolean> => {
-  return bcrypt.compare(password, receivedPassword);
-};
+  static comparePasswords(
+    password: string,
+    receivedPassword: string
+  ): Promise<boolean> {
+    return bcrypt.compare(password, receivedPassword);
+  }
+}
