9c1e9aa9159298c14ecfb0528cf54d0ec8c74a15
jhonashc <jhonhuiracocha123456789@gmail.com>
2023-11-02 23:54:13 -0500

    multer middleware updated: now you can only upload files of maximum 5MB

--- src/middlewares/multer.middleware.ts
+++ src/middlewares/multer.middleware.ts
@@ -4,7 +4,10 @@ import { MulterHelper } from "../helpers";
 
 const storage = multer.memoryStorage();
 
+const maxSize = 5 * 1024 * 1024; /* 5MB */
+
 export const uploader = multer({
   storage,
   fileFilter: MulterHelper.fileFilter,
+  limits: { fileSize: maxSize },
 });
