c02ac6ca00fd36f0d1bcca21bce95eabcabca42a
jhonashc <jhonhuiracocha123456789@gmail.com>
2023-11-02 23:55:11 -0500

    exception handler updated: multer exceptions are now also handled

--- src/middlewares/exception-handler.middleware.ts
+++ src/middlewares/exception-handler.middleware.ts
@@ -1,5 +1,7 @@
 import { NextFunction, Request, Response } from "express";
 
+import { MulterError } from "multer";
+
 import { CustomError } from "../errors/custom.error";
 
 export const exceptionHandler = (
@@ -8,17 +10,29 @@ export const exceptionHandler = (
   res: Response,
   next: NextFunction
 ) => {
+  const defualtMessage: string =
+    "Algo salió mal, inténtalo de nuevo más tarde.";
+
   if (error instanceof CustomError) {
     const { message, statusCode } = error;
 
     return res.status(statusCode).json({
       status: false,
-      message: message.split("|"),
+      message: message.includes("|") ? message.split("|") : message,
+    });
+  } else if (error instanceof MulterError) {
+    const isFileSize: boolean = error.code === "LIMIT_FILE_SIZE";
+
+    return res.status(400).json({
+      status: false,
+      message: isFileSize
+        ? "Lo sentimos, no es posible subir archivos de más de 5MB."
+        : defualtMessage,
     });
   }
 
   return res.status(500).json({
     status: false,
-    message: "Algo salió mal, inténtalo de nuevo más tarde.",
+    message: defualtMessage,
   });
 };
