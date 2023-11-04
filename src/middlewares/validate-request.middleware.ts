0efb6ccba4ffb5032ee56df2bce39764b5bad882
jhonashc <jhonhuiracocha123456789@gmail.com>
2023-11-04 04:37:25 -0500

    middlewares updated: joi helper implemented in validate file and request middleware added

--- src/middlewares/validate-request.middleware.ts
+++ src/middlewares/validate-request.middleware.ts
@@ -2,6 +2,8 @@ import { NextFunction, Request, Response } from "express";
 
 import { Schema } from "joi";
 
+import { JoiHelper } from "../helpers";
+
 import { CustomError } from "../errors/custom.error";
 
 export type RequestType = "body" | "params" | "query";
@@ -9,17 +11,12 @@ export type RequestType = "body" | "params" | "query";
 export const validateRequest = (schema: Schema, requestType: RequestType) => {
   return (req: Request, res: Response, next: NextFunction) => {
     try {
-      const { error } = schema.validate(req[requestType]);
+      const message: string | null = JoiHelper.validate(
+        schema,
+        req[requestType]
+      );
 
-      if (error) {
-        const { details } = error;
-
-        const message: string = details
-          .map((detail) => detail.message.replace(/['"]/g, ""))
-          .join("|");
-
-        throw CustomError.badRequest(message);
-      }
+      if (message) throw CustomError.badRequest(message);
 
       next();
     } catch (error) {
