01c498ca5326ea8e97eaa060eb738d3b9b1e4de2
JimyCoxRocha <terraxted@hotmail.com>
2023-11-03 23:01:55 -0500

    update: port value change

--- src/config/envs.config.ts
+++ src/config/envs.config.ts
@@ -3,7 +3,7 @@ import { get } from "env-var";
 
 export const envs = {
   NODE_ENV: get("NODE_ENV").required().asString(),
-  PORT: get("PORT").required().asPortNumber(),
+  PORT: get("PORT").asPortNumber(),
   DATABASE_URL: get("DATABASE_URL").required().asString(),
   MYSQL_ROOT_PASSWORD: get("MYSQL_ROOT_PASSWORD").asString(),
   MYSQL_DATABASE: get("MYSQL_DATABASE").asString(),
